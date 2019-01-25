9930598ba7cc674a1e18338dfcafbeb86476e8d7
rudy <arizafyrudy@gmail.com>
2019-01-25 21:51:10 +0100

    glide

--- app/src/main/java/com/example/digi_move/accueil.kt
+++ app/src/main/java/com/example/digi_move/accueil.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.firebase.ui.auth.AuthUI
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.*
@@ -21,7 +22,7 @@ class accueil : AppCompatActivity() {
 		textView_bienvenu.text = user?.email.toString()
 		Toast.makeText(baseContext, "${user?.email}  ${user?.uid}",
 			Toast.LENGTH_LONG).show()
-
+		 Glide.with(this).load(user?.photoUrl).into(nv_icon)
 		btn_deconnexion.setOnClickListener {
 			logout()
 		}
